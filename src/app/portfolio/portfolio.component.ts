98044c2ce7eb86c3fa17d9f57861b98d815bff87
Simon Funke <sim_89@web.de>
2024-02-19 08:01:52 +0100

    added two projects

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
+import { Projects } from './../shared/interfaces/projects';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [ProjectComponent, CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
 
   title = 'My Projects';
+  projects: Projects[] = [];
+
+  constructor() {
+    this.projects.push(
+      {
+        name: 'El-Pollo-Loco',
+        discription: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+        image: './../assets/img/el-pollo-loco.png',
+      }
+    );
+
+    this.projects.push(
+      {
+        name: 'Join',
+        discription: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+        image: './../assets/img/join.png',
+      }
+    );
+  }
 
 }
