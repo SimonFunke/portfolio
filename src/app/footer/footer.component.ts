39b71bdd968863adc5a2053dda8538d1f5a08fd5
Simon Funke <sim_89@web.de>
2024-02-20 13:42:22 +0100

    added footer

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  isActive: boolean = false;
+
+  constructor(private router:Router) {
+    
+  }
+
+  @Input() showImages: boolean = true;
+
+  goToGit(){
+    window.open('https://github.com/SimonFunke/', '_blank');
+  }
+
+}
