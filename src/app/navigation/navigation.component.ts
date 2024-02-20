5e5abe6b9b3d1aff3869afd3cf6fc3126ae8ebf3
Simon Funke <sim_89@web.de>
2024-02-20 10:55:25 +0100

    added routerLinkActive

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
-
-  constructor() { 
-    
-  }
 
 }
