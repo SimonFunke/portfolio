021b2d08b0ed6d4831b4093493514670e1307972
Simon Funke <sim_89@web.de>
2024-02-20 13:42:57 +0100

    added footer

--- src/app/intern/intern.component.ts
+++ src/app/intern/intern.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from '../navigation/navigation.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-intern',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [RouterOutlet, NavigationComponent, FooterComponent],
   templateUrl: './intern.component.html',
   styleUrl: './intern.component.scss'
 })
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
 
   isActive: boolean = false;
+  showImages: boolean = false;
 
   constructor(private router:Router) {
   }
