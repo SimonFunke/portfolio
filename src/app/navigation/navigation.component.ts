a8bd6f968dbdc07ad047fd049553801336ea1c10
Simon Funke <sim_89@web.de>
2024-02-21 12:12:44 +0100

    added

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
@@ -5,10 +6,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+
+  mobileMenu: boolean = false;
+  burgerIconSrc: string = './assets/icons/burger.png';
+  constructor() { }
+
+  toggleMenu(){
+    this.mobileMenu = !this.mobileMenu;
+    if(this.mobileMenu){
+      this.burgerIconSrc = './assets/icons/burger_x.png';
+    }else{
+      this.burgerIconSrc = './assets/icons/burger.png';
+    }
+  }
+
+  closeMenu(){
+    this.mobileMenu = false;
+    this.burgerIconSrc = './assets/icons/burger.png';
+  }
 
 }
