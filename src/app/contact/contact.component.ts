8eaab4c21cd27e98de611b060366687eeaed5367
Simon Funke <sim_89@web.de>
2024-03-01 01:05:55 +0100

    added send mail

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -23,7 +24,23 @@ export class ContactComponent {
     message: '',
   };
 
-  constructor(private router:Router) { }
+  mailTest: boolean = false;
+
+  post = {
+    endPoint: 'https://simon-funke.developerakademie.net/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      }
+    }
+  };
+
+  http = inject(HttpClient);
+
+
+  constructor(private router: Router) { }
 
   changeCheckboxImage() {
     this.checked = !this.checked;
@@ -31,22 +48,38 @@ export class ContactComponent {
       this.checkboxImage = './assets/icons/check_box_checked.svg';
     } else {
       this.checkboxImage = './assets/icons/check_box.svg';
-    }   
+    }
   }
 
-  onSubmit(ngForm: NgForm){
-    if(ngForm.valid && ngForm.submitted && this.checked) {
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted && this.checked && !this.mailTest) {
+      this.http
+        .post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+            ngForm.resetForm();
+            this.changeCheckboxImage();
+            this.sendMessage = true;
+            this.sendMessageBox = true;
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+      setTimeout(() => {
+        this.sendMessage = false;
+      }, 2000);
+    }
+    else if (ngForm.valid && ngForm.submitted && this.checked && this.mailTest) {
       ngForm.resetForm();
       this.changeCheckboxImage();
       this.sendMessage = true;
       this.sendMessageBox = true;
-      setTimeout(() => {
-        this.sendMessage = false;
-      }, 2000);
-    }    
+    }
   }
 
-  openPrivacy(){
+  openPrivacy() {
     this.router.navigate(['privacy']);
   }
 }
